0bb3223a28c3466556320db2a0407e176b977d90
2013250433 <shiplight@korea.ac.kr>
2018-05-29 11:10:22 +0900

    change util.js

--- Util.js
+++ Util.js
@@ -10,8 +10,7 @@ export default class Util() extends Component{
 		try{
 			let value = await AsyncStorage.getItem('@complete');
 			if(value == 'true_jin'){
-				//alert('참');
-				this.setState({completeSwitch:true});
+				
 			}
 			else{
 				alert('거짓');
